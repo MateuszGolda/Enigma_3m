846d669c558a6c16b4624c977bdea67d9c9969f1
MateuszGolda <mateusz.golda1993@gmail.com>
2020-02-24 14:26:46 +0100

    fix Enigma.java

--- Enigma.java
+++ Enigma.java
@@ -3,55 +3,50 @@ import java.util.Scanner;
  * Enigma
  */
 public class Enigma {
-    String[] CiphersToChoose = {"Atbash Cipher", "ROT13 Cipher", "Caesar Cipher", "Affine Cipher", "Rail-fence Cipher", "Baconian Cipher"};
-    public void printingCiphers(){
-        for (int i = 0; i < CiphersToChoose.length; i++){
-            System.out.println((i + 1) + " " + CiphersToChoose[i]);}
+    String[] cipherNames = {"Atbash Cipher", "ROT13 Cipher", "Caesar Cipher", "Affine Cipher", "Rail-fence Cipher", "Baconian Cipher"};
+
+    public static void main(String[] args) {
+        final Enigma enigma = new Enigma();
+        enigma.printCiphers();
+        enigma.chooseCipher();
     }
-    public void ChooseCipher(){
+
+    public void printCiphers() {
+        for (int i = 0; i < cipherNames.length; i++) {
+            System.out.println((i + 1) + " " + cipherNames[i]);
+        }
+    }
+
+    public void chooseCipher() {
         System.out.println("Choose your Cipher:");
         try (Scanner input = new Scanner(System.in)){
             final String userChoice = input.next();
-            if (userChoice.equals("1")) {System.out.println("a");}
-            else if (userChoice.equals("2")) {ChooseCrypting();}
-            else if (userChoice.equals("3")) {ChooseCrypting();}
-            else if (userChoice.equals("4")) {ChooseCrypting();}
-            else if (userChoice.equals("5")) {ChooseCrypting();}
-            else if (userChoice.equals("6")) {ChooseCrypting();}
-            else {ChooseCipher();}
+            if (userChoice.equals("1")) {chooseCrypting();}
+            else if (userChoice.equals("2")) {chooseCrypting();}
+            else if (userChoice.equals("3")) {chooseCrypting();}
+            else if (userChoice.equals("4")) {chooseCrypting();}
+            else if (userChoice.equals("5")) {chooseCrypting();}
+            else if (userChoice.equals("6")) {chooseCrypting();}
+            else {chooseCipher();}
+        }
     }
-    }
-    public void ChooseCrypting() {
+
+    public void chooseCrypting() {
         System.out.println("To encrypt choose 1, to decrypt choose 2");
         System.out.println("Enter your choice");
         try (Scanner secondInput = new Scanner(System.in)) {
             final String userChoice = secondInput.next();
-            if (userChoice.equals("1")) {AtbashCipher.decrypt(TextToGive());}
-            else if (userChoice.equals("2")) {AtbashCipher.encrypt(TextToGive());}
-            else {ChooseCrypting();}
+            if (userChoice.equals("1")) {System.out.println(AtbashCipher.decrypt(getUserInput()));}
+            else if (userChoice.equals("2")) {System.out.println(AtbashCipher.encrypt(getUserInput()));}
+            else {chooseCrypting();}
         }
     }
-    
-    public String TextToGive() {
+
+    public String getUserInput() {
         System.out.println("Enter your text");
         try (Scanner thirdInput = new Scanner(System.in)) {
             final String userInput = thirdInput.next();
             return userInput;
-        }  
-    } 
-
-    
-    public void printDecrypted() {
-        System.out.println(AtbashCipher.decrypt(TextToGive()));
-    }
-    public void printEncrypted() {
-        System.out.println(AtbashCipher.Encrypt(TextToGive()));
-    }
-    public static void main(String[] args) {
-        final Enigma enigma = new Enigma();
-        enigma.printingCiphers();
-        enigma.ChooseCipher();
-        enigma.printDecrypted();
-        enigma.printEncrypted();
+        }
     }
 }
