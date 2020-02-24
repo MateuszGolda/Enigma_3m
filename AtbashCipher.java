022e96f6b871e93d163800e8046f6456446e18cb
MateuszGolda <mateusz.golda1993@gmail.com>
2020-02-24 13:28:15 +0100

    Implement atbash cipher

--- AtbashCipher.java
+++ AtbashCipher.java
@@ -4,12 +4,19 @@
 public class AtbashCipher {
 
     public static String encrypt(String decrypted) {
-        String encrypted = "encrypted " + decrypted;
+        String abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String zyx = "zyxwvutsrqponmlkjihgfedcbaZYXWVUTSRQPONMLKJIHGFEDCBA";
+
+        String encrypted = "";
+        for (int i = 0; i < decrypted.length(); i++) {
+            String charr = String.valueOf(decrypted.charAt(i));
+            encrypted += zyx.charAt(abc.indexOf(charr));
+        }
         return encrypted;
     }
 
     public static String decrypt(String encrypted) {
-        String decrypted = "decrypted " + encrypted;
+        String decrypted = encrypt(encrypted);
         return decrypted;
     }
 
--- Enigma.java
+++ Enigma.java
@@ -4,6 +4,7 @@
 public class Enigma {
 
     public static void main(String[] args) {
-        
+        String decrypted = AtbashCipher.decrypt("Abcxyz");
+        System.out.println(decrypted);
     }
 }
