64b4acb374ec8556eddcef914a8c30a481d77919
Jadwiga Skarbek <jadwiga.skarbek@MBP-Dominik.local>
2020-02-27 13:10:32 +0100

    add first decrypt feature

--- RailFenceCipher.java
+++ RailFenceCipher.java
@@ -2,7 +2,10 @@ import java.util.ArrayList;
 /*
 0481357926
 */
+
 public class RailFenceCipher {
+    public static int iteration = 0;
+
     public static String encrypt(String toEncrypt) {
             ArrayList<String> changed = new ArrayList<>();
             firstLine(toEncrypt, changed);
@@ -13,14 +16,15 @@ public class RailFenceCipher {
             sb.append(s);
         return sb.toString();
     }
-
+   
     public static String decrypt(String toEncrypt) {
-        ArrayList<String> changed = new ArrayList<>();
-        firstLine(toEncrypt, changed);
-        secondLine(toEncrypt, changed);
-        thirdLine(toEncrypt, changed);
+        String[] decrypted = new String[toEncrypt.length()];
+        firstTurn(toEncrypt, decrypted);
+        secondTurn(toEncrypt, decrypted);
+        thirdTurn(toEncrypt, decrypted);
+        
         StringBuilder sb = new StringBuilder();
-    for (String s : changed)
+    for (String s : decrypted)
         sb.append(s);
     return sb.toString();
     }
@@ -38,7 +42,7 @@ public class RailFenceCipher {
             }
         }
 
-
+       
         public static void secondLine(String toEncrypt, ArrayList<String> changed) {
             int i = 1;
             while (i < toEncrypt.length()) {
@@ -65,5 +69,47 @@ public class RailFenceCipher {
                 }
             }
         }
+        public static void firstTurn(String toEncrypt, String[] decrypted) {
+            int i = 0;
+            while (i < toEncrypt.length()) {
+                if (i < toEncrypt.length()){
+                    decrypted[i] = toEncrypt.substring(i, i+1);
+                    i+=4;
+                    iteration+=1;
+                }
+                if (i > toEncrypt.length()){
+                    break;
+                }
+            }
+        }
 
+
+        public static void secondTurn(String toEncrypt, String[] decrypted) {
+            int i = iteration;
+            while (i < toEncrypt.length()) {
+                if (i < toEncrypt.length()){
+                    decrypted[iteration] = "-"; /*toEncrypt.substring(iteration, iteration+1);*/
+                    i+=2;
+                    iteration+=1;
+                }
+                if (i > toEncrypt.length()){
+                    break;
+                }
+            }
+        }
+
+
+        public static void thirdTurn(String toEncrypt, String[] decrypted) {
+            int i = iteration;
+            while (i < toEncrypt.length()) {
+                if (i < toEncrypt.length()){
+                    decrypted[iteration] = "-";  /*toEncrypt.substring(iteration, iteration+1);*/
+                    i+=4;
+                    iteration+=1;
+                }
+                if (i > toEncrypt.length()){
+                    break;
+                }
+            }
+        }
         }
