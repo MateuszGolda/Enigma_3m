a351e670ba38f508d0610f1100becd9e218fb449
Jadwiga Skarbek <jadwiga.skarbek@MBP-Dominik.local>
2020-02-27 10:55:02 +0100

    add encrypting function

---
+++ RailFenceCipher.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+/*
+0481357926
+*/
+public class RailFenceCipher {
+    public static String encrypt(String toEncrypt) {
+            ArrayList<String> changed = new ArrayList<>();
+            firstLine(toEncrypt, changed);
+            secondLine(toEncrypt, changed);
+            thirdLine(toEncrypt, changed);
+            StringBuilder sb = new StringBuilder();
+        for (String s : changed)
+            sb.append(s);
+        return sb.toString();
+    }
+
+    public static String decrypt(String toEncrypt) {
+        ArrayList<String> changed = new ArrayList<>();
+        firstLine(toEncrypt, changed);
+        secondLine(toEncrypt, changed);
+        thirdLine(toEncrypt, changed);
+        StringBuilder sb = new StringBuilder();
+    for (String s : changed)
+        sb.append(s);
+    return sb.toString();
+    }
+
+        public static void firstLine(String toEncrypt, ArrayList<String> changed) {
+            int i = 0;
+            while (i < toEncrypt.length()) {
+                if (i < toEncrypt.length()){
+                    changed.add(toEncrypt.substring(i, i+1));
+                    i+=4;
+                }
+                if (i > toEncrypt.length()){
+                    break;
+                }
+            }
+        }
+
+
+        public static void secondLine(String toEncrypt, ArrayList<String> changed) {
+            int i = 1;
+            while (i < toEncrypt.length()) {
+                if (i < toEncrypt.length()){
+                    changed.add(toEncrypt.substring(i, i+1));
+                    i+=2;
+                }
+                if (i > toEncrypt.length()){
+                    break;
+                }
+            }
+        }
+
+
+        public static void thirdLine(String toEncrypt, ArrayList<String> changed) {
+            int i = 2;
+            while (i < toEncrypt.length()) {
+                if (i < toEncrypt.length()){
+                    changed.add(toEncrypt.substring(i, i+1));
+                    i+=4;
+                }
+                if (i > toEncrypt.length()){
+                    break;
+                }
+            }
+        }
+
+        }
